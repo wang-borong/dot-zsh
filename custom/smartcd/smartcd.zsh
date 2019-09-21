642cc843b35b4499cd930cbbeeb00f758a580b5f
Jason Wang <12841205+bestwbr@users.noreply.github.com>
2019-09-21 20:51:37 +0800

    Update smartcd.zsh

--- custom/smartcd/smartcd.zsh
+++ custom/smartcd/smartcd.zsh
@@ -1,4 +1,5 @@
 # - Smart cd
+SCRIPT_DIR=$(dirname $0)
 cd() {
     if [[ -e $1 ]]; then
         if [[ ! -d $1 ]]; then
@@ -7,7 +8,7 @@ cd() {
             builtin cd $1 && ls --color=tty
         fi
     elif [[ -n $1 ]]; then
-        dir=$(python3 $ZSH/custom/smartcd/smartcd.py $1)
+        dir=$(python3 $SCRIPT_DIR/smartcd.py $1)
         #print "correct $(tput setaf 1)${1}${reset}$(tput sgr0) to $(tput setaf 2)${dir}$(tput sgr0)"
         builtin cd $dir &&
         ls --color=tty
