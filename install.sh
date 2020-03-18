ff0a324aa0270d62fb92cdf5c6aa0c156bd3c92e
Wangborong <wang_borong@163.com>
2020-03-18 21:44:54 +0800

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
-[[ ! -d external ]] && mkdir external && cd external
+[[ ! -d external ]] && mkdir external
+cd external
 git clone https://github.com/zsh-users/zsh-completions.git
 git clone https://github.com/skywind3000/z.lua
 git clone https://github.com/seebi/dircolors-solarized
