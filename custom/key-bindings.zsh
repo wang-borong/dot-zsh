75e3bf4bc79fe337764fa8fc0b774377200248a5
Jason Wang <12841205+bestwbr@users.noreply.github.com>
2020-07-31 11:05:55 +0800

    Add bindkey to compile codes

--- custom/key-bindings.zsh
+++ custom/key-bindings.zsh
@@ -1,3 +1,5 @@
 # bind Alt-t to start tmux
 bindkey -s '^[t' 'tmux\n'
 bindkey -s '^[T' 'builtin cd ~; tmux new\n'
+# compile your code by hit Alt+Shift+c
+bindkey -s '^[C' 'tmux split-window -h "bash ~/Workspace/tools/tmux_compile.sh; read"\n'
